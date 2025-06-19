8b182d031b69465263a1a9720914e30a2e30d50f
Eren Meral <erenmeral02@gmail.com>
2025-06-19 18:38:57 +0300

    ilk uygulama

---
+++ ilk_uygulama.cs
@@ -0,0 +1,8 @@
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Eren Meral");
+        Console.WriteLine("Kendimi backend development alanında geliştirmeye çalışıyorum.");
+    }
+}
